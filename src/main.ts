35f10b7b0b61d2ee8426a67d20218f12a77b8d7e
Joey Li <joey@clarifyhealth.com>
2017-12-07 17:12:29 +0800

    change upgrade to downgrade

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { enableProdMode, StaticProvider } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { downgradeModule, downgradeComponent, setAngularLib } from '@angular/upgrade/static';
+import { environment } from './environments/environment';
+import {
+  AppModule
+} from './app/app.module';
+import { AddPinComponent } from './app/add-pin/add-pin.component';
+import './js/app';
+
+declare var angular: any;
+
+const bootstrapFn = (extraProviders: StaticProvider[]) => {
+  const platformRef = platformBrowserDynamic(extraProviders);
+  return platformRef.bootstrapModule(AppModule);
+};
+
+const downgradedModule = downgradeModule(bootstrapFn);
+
+angular.module('ngInterestApp', ['interestApp', downgradedModule]).directive('addPin', downgradeComponent({ component: AddPinComponent }));
+
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// /*
+//  * Bootstrap the App
+//  */
+// upgradeAdapter.bootstrap(document.body, ['interestApp']);
+
+// // -----------------
+// // (ignore this, sorry)
+// // HACK: horrible workaround for AoT bootstrap detection bug
+// // https://github.com/angular/angular-cli/issues/3540#issuecomment-270627460
+// const hackThis = false;
+// if (hackThis) {
+//   platformBrowserDynamic().bootstrapModule(AppModule);
+// }
+angular.bootstrap(document.body, ['ngInterestApp']);
---
+++ src/typings.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="./js/app.d.ts"/>
+
+/* SystemJS module definition */
+declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+}
